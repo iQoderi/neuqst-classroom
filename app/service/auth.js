c69474d81a290204bf434d625335d60b83c773ea
qoder <qoder@hrsoft.net>
2017-05-14 20:47:14 +0800

    Add userExist service

--- app/service/auth.js
+++ app/service/auth.js
@@ -2,6 +2,11 @@
 
 module.exports = app => {
     class AuthService extends app.Service {
+        async userExist (id) {
+            const result = await this.app.mysql.query('user',{ id });
+            return result;
+        }
+
         async register(user) {
             const result = await this.app.mysql.insert('user', user);
             return result;
