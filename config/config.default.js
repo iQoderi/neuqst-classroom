0f5479136f22e61c12bc272dc73ae77749738271
lethe <1443646652@qq.com>
2017-05-18 21:04:47 +0800

    Add pageItem apge

--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = appInfo => {
   const config = {
     view: {
@@ -9,6 +11,14 @@ module.exports = appInfo => {
         },
     },
     middleware: ['errorPage'],
+    static: {
+        prefix: '/public/',
+        dir: path.join(appInfo.baseDir, 'app/public'),
+        // support lazy load
+        dynamic: true,
+        preload: false,
+        buffer: false,
+    },
     keys: appInfo.name + '_1494245470526_4357',
   };
 
