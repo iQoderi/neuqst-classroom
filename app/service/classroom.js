82aca6251d91b3ec31e74511fb6696c43de6a99e
qoder <qoder@hrsoft.net>
2017-05-19 23:49:12 +0800

    Add classRoom service

--- app/service/classroom.js
+++ app/service/classroom.js
@@ -11,6 +11,21 @@ module.exports = app => {
             const result = await this.app.mysql.get('classroom', { id });
             return result;
         }
+
+        async update(row) {
+            const result = await this.app.mysql.update('classroom', row);
+            return result.affectedRows === 1;
+        }
+
+        async create(row) {
+            const result = await this.app.mysql.insert('classroom', row);
+            return result.affectedRows === 1;
+        }
+
+        async remove(id) {
+            const result = await this.app.mysql.delete('classroom', { id });
+            return result.affectedRows === 1;
+        }
     }
 
     return ClassroomService;
