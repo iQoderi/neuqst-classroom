08b87b66a37715fdd64b22b1088efffbefba8b6a
qoder <qoder@hrsoft.net>
2017-05-23 17:31:02 +0800

    Update get admin api

--- app/service/admin.js
+++ app/service/admin.js
@@ -3,7 +3,19 @@
 module.exports = app => {
     class AdminService extends app.Service {
         async list () {
-            const result = await this.app.mysql.get('user', { role: 3 });
+            const result = await this.app.mysql.select('user',{
+                where: { role: 3 },
+                columns: [ 'id', 'name', 'email', 'role', 'college', 'major', 'class', 'code', 'isActive'],
+            });
+            return result;
+        }
+
+        async detail(id) {
+            const query = {
+                id,
+                role: 3,
+            };
+            const result = await this.app.mysql.get('user', query);
             return result;
         }
     }
