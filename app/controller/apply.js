d07109a705a9f9ee926c6dc784f7cec1999483a0
qoder <qoder@hrsoft.net>
2017-05-25 20:50:57 +0800

    Add create apply api

--- app/controller/apply.js
+++ app/controller/apply.js
@@ -14,7 +14,41 @@ module.exports = app => {
         }
 
         async create(ctx) {
-            ctx.body = 'create';
+            const { body } = ctx.request;
+            const { classroomId } = body;
+            const classroom = await ctx.service.classroom.detail(classroomId);
+            if (!classroom) {
+                return ctx.body = {
+                    code: 20002,
+                }
+            }
+            const { status, name: classname, imgUrl: classImg, place: classPlace } = classroom;
+            if (status === 1) {
+                return ctx.body = {
+                    code: 20003,
+                }
+            }
+            const [createAt, updateAt] = [Date.now(), Date.now()];
+            const { id: uid, name: uname, role: urole, phone: uphone} = ctx.state.user;
+            const apply = Object.assign(body, {
+                createAt,
+                updateAt,
+                uid,
+                uname,
+                urole,
+                uphone,
+                classname,
+                classImg,
+                classPlace,
+            });
+            const isSuccess = await ctx.service.apply.create(apply);
+            if (isSuccess) {
+                return ctx.body = {
+                    code: 0,
+                }
+            }
+
+            // ctx.body = 'create';
         }
 
         async show(ctx) {
@@ -29,7 +63,16 @@ module.exports = app => {
         }
 
         async destroy(ctx) {
-            ctx.body = 'delete';
+            const { id } = ctx.params;
+            const isSuccess = await ctx.service.apply.remove(id);
+            if (isSuccess) {
+                return ctx.body = {
+                    code: 0,
+                };
+            }
+            ctx.body = {
+                code: 40001,
+            }
         }
 
         async update(ctx) {
--- app/router/apply.js
+++ app/router/apply.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const checkToken = require('../middleware/checkToken');
+
 module.exports = app => {
-    app.resources('apply', '/apply', 'apply');
+    app.get('/apply', 'apply.index');
+    app.get('/apply/:id', 'apply.show');
+    app.delete('/apply/:id',  checkToken(app), app.auth.hasRole(3), 'apply.destroy');
+    app.post('/apply', checkToken(app), 'apply.create');
+    // app.resources('apply', '/apply', 'apply');
 };
