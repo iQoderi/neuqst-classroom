7991c53f46b929a8968a1f2bd0b04dcb3ef14086
qoder <qoder@hrsoft.net>
2017-05-25 21:39:10 +0800

    Init antd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import YeomanImage from './YeomanImage';
 import './app.css';
 
 class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index">
-        <YeomanImage />
-        <div className="notice">
-          Please edit <code>src/components/App.js</code> to get started!
-        </div>
+      <div>
+        index
       </div>
     );
   }
